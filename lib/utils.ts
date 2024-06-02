58ab5f2510900db8e85b52024751c3239588fd14
Rafa Moreno <rmoreno@tinybird.co>
2024-06-02 23:25:57 +0200

    bug

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,7 +13,7 @@ export function useDateRange() {
     date_from: string | undefined
     date_to: string | undefined
   }>({
-    date_from: format(subDays(new Date(), 7), 'yyyy-MM-dd'),
+    date_from: format(subDays(new Date(), 1), 'yyyy-MM-dd'),
     date_to: format(new Date(), 'yyyy-MM-dd'),
   })
 }
