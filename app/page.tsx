983fe29071a270addf075fe06f5fd688723de50c
Rafa Moreno <rmoreno@tinybird.co>
2024-06-02 22:28:11 +0200

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 'use client'
 
-import { useState } from 'react'
-import { subDays } from 'date-fns/subDays'
 import { DateRangePicker } from '@/components/ui/DateRangePicker'
-import { format } from 'date-fns/format'
+import { useDateRange } from '@/lib/utils'
 
 export default function Home() {
-  const [dateRange, setDateRange] = useState({
-    date_from: format(subDays(new Date(), 7), 'yyyy-MM-dd'),
-    date_to: format(new Date(), 'yyyy-MM-dd'),
-  })
+  const [dateRange, setDateRange] = useDateRange()
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-8">
       <div className="flex justify-end w-full">
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { format } from 'date-fns/format'
+import { subDays } from 'date-fns/subDays'
+import { useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function useDateRange() {
+  return useState({
+    date_from: format(subDays(new Date(), 7), 'yyyy-MM-dd'),
+    date_to: format(new Date(), 'yyyy-MM-dd'),
+  })
 }
