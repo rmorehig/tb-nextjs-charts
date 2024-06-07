3f6938c1f3e3251d429a32c24c424dbb03cf2108
Rafa Moreno <rmoreno@tinybird.co>
2024-06-07 16:35:56 +0200

    remove pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 'use client'
 
-import { TopDevices } from '@/components/TopDevices'
-import { TopPages } from '@/components/TopPages'
-import { UniqueVisitors } from '@/components/UniqueVisitors'
 import { DateRangePicker } from '@/components/ui/DateRangePicker'
 import { useDateParams } from '@/lib/utils'
 
 export default function Home() {
   const [dateParams, setDateParams] = useDateParams()
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-8 max-w-[1400px] mx-auto">
       <div className="flex justify-end w-full">
         <DateRangePicker dateParams={dateParams} onChange={setDateParams} />
       </div>
-      <UniqueVisitors {...dateParams} />
-      <div className="flex gap-8 w-full">
-        <TopDevices {...dateParams} />
-        <TopPages limit={4} {...dateParams} />
-      </div>
+      <div className="flex gap-8 w-full"></div>
+      <div className="flex gap-8 w-full"></div>
     </main>
   )
 }
