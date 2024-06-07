a8c2c64bb9ba35f006d85999487960c00d5275ff
Rafa Moreno <rmoreno@tinybird.co>
2024-06-07 17:53:08 +0200

    fix height

--- components/TopSources.tsx
+++ components/TopSources.tsx
@@ -18,7 +18,7 @@ export function TopSources(params: {
       borderRadius="8px"
       boxShadow="0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)"
       title="Top sources"
-      height="500px"
+      height="300px"
       padding="16px"
       params={params}
     />
